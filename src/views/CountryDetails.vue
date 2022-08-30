c23e658fafacdfefc759267be4104dec0006afb4
Maria Cassandra <maria.cassandra@gmail.com>
2022-08-30 17:34:16 +0200

    update

--- src/views/CountryDetails.vue
+++ src/views/CountryDetails.vue
@@ -1,10 +1,12 @@
 <template>
   <div class="countries-details--container">
     <img :src="`https://flagpedia.net/data/flags/icon/72x54/${countryInfo.alpha2Code.toLowerCase()}.png`" :alt="`Flag of the ${countryInfo.name}`">
-    <h1>Esto es Country Details</h1>
+    <h1>Here is the Country Details</h1>
     <p>Capital: {{ countryInfo.capital }}</p>
     <p>Area: {{ countryInfo.area}} Km2</p>
-    <router-link v-for="border in countryInfo.borders" :key="border" :to="border">{{ border }}</router-link>
+    <router-link v-for="border in countryInfo.borders" :key="border" :to="border">{{ border }}
+      <ul> {{ border }} </ul>
+    </router-link>
   </div>
 </template>
 
